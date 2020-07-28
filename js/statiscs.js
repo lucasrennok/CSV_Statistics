93d1aa901d076f40deef4eeaedfc72638432222c
Lucas Rennó Kallás <lucasrk@unifei.edu.br>
2020-07-28 16:24:29 -0300

    Text drop & drag

--- js/statiscs.js
+++ js/statiscs.js
@@ -2,6 +2,8 @@ var element = document.querySelector('.droppable');
 
 function callback(files) {
   console.log(files);
+  let drop = document.getElementById("drop_f");
+  drop.innerHTML = files.length+" Files Selected";
 }
 
 makeDroppable(element, callback);
