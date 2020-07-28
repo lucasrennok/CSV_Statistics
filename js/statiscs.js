302afc0572ebbff0fc0e082d6750c2eef78959d4
Lucas Rennó Kallás <lucasrk@unifei.edu.br>
2020-07-28 01:40:42 -0300

    div disappear

--- js/statiscs.js
+++ js/statiscs.js
@@ -1,1 +1,12 @@
-/* nothing yet */
+let upload = document.getElementById("upload");
+
+upload.addEventListener("click", uploadData);
+
+function uploadData(){
+    /* se nao tiver documento = window.alert
+        se tiver 1 só documento: gera graficos e tabela
+        se tiver 2 ou mais: verifica se tem os msms atributos:
+                                se sim: gera graficos e tabela
+                                senão: window.alert*/
+    document.getElementById("upload_div").style.display = "none";
+}
