d1ae3efc243b9b2188f28309cba218e287344a33
Lucas Rennó Kallás <lucasrk@unifei.edu.br>
2020-07-29 03:36:49 -0300

    Building the data

--- js/statiscs.js
+++ js/statiscs.js
@@ -1,19 +1,28 @@
 let drop_area = document.querySelector('.droppable');
 let upload_button = document.getElementById("upload");
 
+// Read Files and Check
 FileList = undefined;
 let result = 0;
 let file_names = [];
 let csv_data = [];
 upload_button.addEventListener("click", uploadFiles);
 
+// Build Data
+let column_vector = [];
+let data_matrix = [];
+
+// Receive the fileList of the drop area
 function callback(files) {
   let drop = document.getElementById("drop_f");
   let number = files.length;
   drop.innerHTML = number + " Files Selected";
   FileList = files;
 }
+// Make the drop area droppable
+makeDroppable(drop_area, callback);
 
+// Check if the files are .csv and are compatible
 const checkFiles = (FileList) => {
   for (let file of FileList) {
     let extension = "" + file.name[file.name.length - 4] + file.name[file.name.length - 3] + file.name[file.name.length - 2] + file.name[file.name.length - 1];
@@ -37,7 +46,7 @@ const checkFiles = (FileList) => {
           result=-1;
           return;
         }
-        console.log(csv_data[csv_data.length-1]);
+        //console.log(csv_data[csv_data.length-1]);
       });
     } else {
       console.log("ERROR: NOT A CSV FILE!");
@@ -48,8 +57,7 @@ const checkFiles = (FileList) => {
   return 0;
 }
 
-makeDroppable(drop_area, callback);
-
+// After clicking on "upload" button=> checkFiles and buildData
 function uploadFiles() {
   if (FileList == undefined) {
     window.alert("No Files");
@@ -63,9 +71,11 @@ function uploadFiles() {
     file_names = [];
     return;
   }
+  window.alert("Wait some seconds...");
+
+  //Wait fileReader to get the strings
   setTimeout(
     function(){
-      window.alert("Wait some seconds...");
       if(result==-1){
         let drop = document.getElementById("drop_f");
         drop.innerHTML = "Put your files here.";
@@ -76,20 +86,20 @@ function uploadFiles() {
         result=0;
         return;
       }
-      console.log("Uploaded");
+      console.log("OUTPUT: _Uploaded Files_");
       
       const upload_div = document.getElementById("upload_div");
-      
       upload_div.style.display = "none";
-      //monta os dados num vetor
+
+      //Build data of the csv
       buildData();
-  },2000);
+  },3000);
 }
 
-//confere compatibilidade dos csvs
+//Check if the csvs area compatible
 function compatibleCSVs() {
   if(csv_data.length>1){
-    //compara ultimo com primeiro
+    //Check the last with the first
     let column1="", column2="";
     for(let word of csv_data[0]){
       if(word=="\n")
@@ -106,14 +116,16 @@ function compatibleCSVs() {
       return false;
     }
   }
+  console.log("OUTPUT: Combatibles!");
   return true;
 }
 
+// Build the data
 function buildData(){
+  console.log("OUTPUT: Building Data...");
+  let contador = 0;
   let aspas = false;
-  let column_vector = [];
   let line_vector = []
-  let data_matrix = []
   let aux = "", column_generated = false;
   for(let i=0; i<csv_data.length; i++){
     for(let j=0; j<csv_data[i].length; j++){
@@ -134,12 +146,27 @@ function buildData(){
           aspas=false;
         }
       }else{
-        
-        console.log(column_vector);
-        return;
+        if(csv_data[i][j]=="\n"){
+          line_vector[column_vector[contador]]=aux;
+          aux = "";
+          data_matrix[data_matrix.length]=line_vector;
+          line_vector = []
+          contador = 0;
+        }else if(csv_data[i][j]=="," && aspas==false){
+          line_vector[column_vector[contador]]=aux;
+          contador++;
+          aux = "";
+        }else if(csv_data[i][j]!="\""){
+          aux+=csv_data[i][j];
+        }else{
+          if(aspas==false)
+          aspas = true;
+          else
+          aspas=false;
+        }
       }
-      //csv_data[i][j];
     }
   }
+  console.log(data_matrix);
   return;
 }
