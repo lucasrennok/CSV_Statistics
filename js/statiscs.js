cf4ec2e29f40c0fbe5c39de7078abccb55f596ea
Lucas Rennó Kallás <lucasrk@unifei.edu.br>
2020-07-30 04:05:13 -0300

    Trying to use Chart

--- js/statiscs.js
+++ js/statiscs.js
@@ -1,25 +1,35 @@
 //this function will receive the data and create the graphs
 const generateGraphs = (statistics) => {
+    let keys = []
+    let values = []
     console.log("Generating Graphics...");
     for(let [col,dat] of statistics){
         console.log("COLUMN NAME: ", col);
         for(let [col2,dat2] of dat){
-            console.log(col2,dat2, "| PERCENT: ", (dat2/dat.size));
+            console.log(col2,dat2, "| PERCENT: ", (dat2/dat.size)," ==> ", dat.size);
+            if(col=="Industry_code_NZSIOC"){
+                keys[keys.length]=col2;
+                values[values.length]=dat2;
+            }
         }
     }
     let element = document.getElementById("myChart");
+    element.style.width = "400px";
+    element.style.height = "800px";
     let chart = new Chart(element, {
         type: 'pie',
         data: {
             datasets:[{
-                label: 'padrao',//column names
-                data: [10,20,30],//values
-                backgroundColor: ['red','blue','black']//color...
+                data: values//,//values
+                //backgroundColor: ['red','blue','black']//color...
             }],
-            labels: ['try','oi','red']//keys
+            labels: keys//keys
         },
         option:{
-
+            title: {
+                text: 'Nome',
+                display: true
+            }
         }
     });
 
@@ -36,7 +46,7 @@ const generateStatistics = (data_matrix,column_vector) => {
         line = new Map();
         for(let j=0; j<data_matrix.length; j++){
             let name_col = data_matrix[j][column_vector[i]];
-            if(line.has(name_col)==false){
+            if(line.has(name_col)===false){
                 line.set(name_col,1);
             }else{
                 line.set(name_col,line.get(name_col)+1);
